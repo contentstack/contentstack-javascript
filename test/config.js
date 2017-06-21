eb4cf38919b5cb9ab707ba9562f108cb310fa2b6
Aamod Pisat <aamod.pisat@raweng.com>
2017-06-21 17:39:37 +0530

    automation script added

--- test/config.js
+++ test/config.js
@@ -10,6 +10,6 @@ module.exports = {
     runscope: {
     	url: "https://api.runscope.com/radar/76730163-fab9-4554-95c1-7ba75aaa2628/trigger?runscope_environment=1f5114ea-7a2d-4adf-9fd3-abc885c47783",
     	username: "aamod.pisat@raweng.com",
-    	password: "cs@aamod$123",
+    	password: "cs@aamod$123"
     }
 };
