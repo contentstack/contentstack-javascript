2327b991ff6a8a02cae092f6e154632fb7cc112b
Aamod Pisat <aamod.pisat@raweng.com>
2017-06-21 12:46:04 +0530

    automation script added

--- test.js
+++ test.js
@@ -1,16 +1,3 @@
 'use strict';
-/*
- * Module Dependencies.
- */
-require('./test/find-v3-stack');
-require('./test/find-result-wrapper-v3-stack');
-require('./test/spread-v3-stack');
-require('./test/findone-v3-stack');
-
-
-//require('./test/dynamic-test');
-//require('./test/find');
-//require('./test/find-result-wrapper');
-//require('./test/findone');
-//require('./test/findone-result-wrapper');
-//require('./test/spread');
+var test  = require('./test/automation-script.js');
+new test();
