c0a86a39f81f837291cdbe9746e77d425c0cc2a3
Aamod Pisat <aamod.pisat@raweng.com>
2017-06-28 10:26:51 +0530

    automation script added and added few more test cases

--- app.js
+++ app.js
@@ -5,29 +5,57 @@ var Contentstack = require('./dist/node/contentstack'),
     app = require('express')(app);
 
 var Stack  = Contentstack.Stack({
-    "api_key": "blt920bb7e90248f607",
-    "access_token": "blt0c4300391e033d4a59eb2857",
-    "environment": "production"
+        "api_key": "blt9fc7b625627e53be",
+        "access_token": "bltf4a8097118277a6e",
+        "environment": "development"
 });
 
 app.get('/', function (req, res, next) {
-   Stack
-       .ContentType('news')
-       .Query()
-       .only(['title', 'body', 'category'])
-       .only('category', 'title')
-       .includeReference('category')
-       .find()
-       .then(function (data) {
-           if(data) {
-               res.send(data);
-           }
-       }).catch(function (err) {
-           console.error("message: ", err);
-           res.send(err)
-       })
-});
+    // var Query = Stack.ContentType('source').Query();
+   // Stack
+   //     .ContentType('source')
+   //     .Query()
+   //     .except('title')
+   //     .find()
+   //     .then(function (data) {
+   //         if(data) {
+   //             res.send(data);
+   //         }
+   //     }).catch(function (err) {
+   //         console.error("message: ", err);
+   //         res.send(err)
+   //     })
 
+    // Query
+    //     .includeReference('reference')
+    //     .only('title')
+    //     // .except('reference', 'title')
+    //     .toJSON().findOne()
+    //     .then(function success(entry) {
+    //         var flag = false;
+    //         if(entry && entry['reference'] && typeof entry['reference'] === 'object') {
+    //             flag = entry.reference.every(function(reference) {
+    //                 return (reference && !("title" in reference));
+    //             });
+    //         }
+    //         res.send(entry);
+    //     }, function error(err) {
+    //         console.error("Error :",err);
+    //         res.send(err);
+    //     });
+
+    var Query = Stack.ContentType('source').Query().includeReference('retailer_reference').includeReference('property').includeReference('category_tag');
+    Query
+    .ascending('created_at')
+    .toJSON()
+    .where('job_fair', true)
+    .find()
+    .then(function success(result) {
+      res.send(result);
+    }, function err(err) {
+      res.send(err);
+    });
+});
 app.listen(process.env.PORT || 8000, function () {
     console.error("App is running on port:", (process.env.PORT || 8000));
 });
