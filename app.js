a9ec72c7b6dc0dae3f6927083fbd883456569f66
Aamod Pisat <aamod.pisat@raweng.com>
2017-06-30 09:47:32 +0530

    js sdk react-native issue fixes

--- app.js
+++ app.js
@@ -4,13 +4,28 @@
 var Contentstack = require('./dist/node/contentstack'),
     app = require('express')(app);
 
-var Stack  = Contentstack.Stack({
-        "api_key": "blt9fc7b625627e53be",
-        "access_token": "bltf4a8097118277a6e",
-        "environment": "development"
-});
+// var Stack  = Contentstack.Stack({
+//         "api_key": "blt9fc7b625627e53be",
+//         "access_token": "bltf4a8097118277a6e",
+//         "environment": "development"
+// });
 
 app.get('/', function (req, res, next) {
+  var Stack = Contentstack.Stack({
+'api_key': 'blt8ca3003060a25f8a',
+'access_token': 'blt460b6594773905171c349a6c',
+'environment': "production"
+});
+
+var Query = Stack.ContentType('property_events').Query().includeReference('retailer_reference').includeReference('property').includeReference('category_tag');
+Query
+.ascending('created_at')
+.toJSON()
+.where('job_fair', true)
+.find()
+.then(function success(result) {
+  res.send(result);
+});
     // var Query = Stack.ContentType('source').Query();
    // Stack
    //     .ContentType('source')
@@ -44,17 +59,17 @@ app.get('/', function (req, res, next) {
     //         res.send(err);
     //     });
 
-    var Query = Stack.ContentType('source').Query().includeReference('retailer_reference').includeReference('property').includeReference('category_tag');
-    Query
-    .ascending('created_at')
-    .toJSON()
-    .where('job_fair', true)
-    .find()
-    .then(function success(result) {
-      res.send(result);
-    }, function err(err) {
-      res.send(err);
-    });
+    // var Query = Stack.ContentType('source').Query().includeReference('retailer_reference').includeReference('property').includeReference('category_tag');
+    // Query
+    // .ascending('created_at')
+    // .toJSON()
+    // .where('job_fair', true)
+    // .find()
+    // .then(function success(result) {
+    //   res.send(result);
+    // }, function err(err) {
+    //   res.send(err);
+    // });
 });
 app.listen(process.env.PORT || 8000, function () {
     console.error("App is running on port:", (process.env.PORT || 8000));
