5157a693cc1d34619946d71a5049c59df2d7950e
Aamod Pisat <aamod.pisat@raweng.com>
2017-09-14 20:53:29 +0530

    bug fix: sdk not working in isomorphic app

--- webpack/webpack.node.js
+++ webpack/webpack.node.js
@@ -5,20 +5,20 @@ var commonConfig = require('./webpack.common.js');
 module.exports = function (options) {
 	return webpackMerge(commonConfig(),{
 		output: {
-		    library: "Contentstack",
-		    libraryTarget:"commonjs2",
-		    path: path.join(__dirname, "../dist/node"),
-		    filename: "contentstack.js"
+			library: "Contentstack",
+			libraryTarget:"commonjs2",
+			path: path.join(__dirname, "../dist/node"),
+			filename: "contentstack.js"
 		},
 		target: "node",
 		resolve: {
 			alias:{
-		      runtime: path.resolve( __dirname,  '../src/runtime/node')
-		    },
+				runtime: path.resolve( __dirname,  '../src/runtime/node')
+			},
 			modules: [
-				'../src', 
-				'../src/runtimes/node',
-				'node_modules', 
+			'../src', 
+			'../src/runtimes/node',
+			'node_modules', 
 			]
 		}
 	});
