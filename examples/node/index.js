c3676ee5454867abbd9b89efb4759806d23b8480
Aamod Pisat <aamod.pisat@raweng.com>
2017-09-15 12:02:24 +0530

    changes n code

--- examples/node/index.js
+++ examples/node/index.js
@@ -1,12 +1,12 @@
 'use strict'
 
-var ContentstackDemo = require('./contentstack-demo.js')
+const ContentstackDemo = require('./contentstack-demo.js')
 
-var Demo = new ContentstackDemo({'api_key': 'bltsomething1234', 'access_token': 'bltsomething1234asdf', 'environment': 'development'})
+const Demo = new ContentstackDemo({'api_key': 'blt950d694ee9fce907', 'access_token': 'blt98a76e10ad802238', 'environment': 'development'})
 
 // get all the entries
 Demo
-    .getEntries('blog')
+    .getEntries('sourcess')
     .then(function (result) {
         console.info("Result1 : ", result)
         // result object with entries
@@ -18,9 +18,9 @@ Demo
 
 // get single entry
 Demo
-    .getEntry('home', 'blt1234567890')
+    .getEntry('source', 'blte9cab9a80ba36d22')
     .then(function (result) {
-        console.info("Result2 : ", result)
+        // console.info("Result2 : ", JSON.stringify(result))
         // result object with entry
     })
     .catch(function (err) {
