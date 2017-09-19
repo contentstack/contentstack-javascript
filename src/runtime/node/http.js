f029782168b66017e7e2e97389336743c4db84fa
Aamod Pisat <aamod.pisat@raweng.com>
2017-09-19 11:08:02 +0530

    Added es6-promise polyfill in browsers.

--- src/runtime/node/http.js
+++ src/runtime/node/http.js
@@ -1,4 +1,6 @@
-require('es6-promise').polyfill();
+import ES6Promise from 'es6-promise';
 import fetch from 'isomorphic-fetch';
+
+ES6Promise.polyfill();
 
 export default fetch;
--- src/runtime/react-native/http.js
+++ src/runtime/react-native/http.js
@@ -1,2 +1,7 @@
+import ES6Promise from 'es6-promise';
+import fetch from 'isomorphic-fetch';
+
+ES6Promise.polyfill();
+
 export default fetch;
 
