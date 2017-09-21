4eabcb5761b763f7f43bd6ab6d1441da7fe6a825
Aamod Pisat <aamod.pisat@raweng.com>
2017-09-21 18:53:47 +0530

    Added es6-promise polyfill in browsers. .spread() promise function

--- src/runtime/react-native/http.js
+++ src/runtime/react-native/http.js
@@ -1,7 +1,3 @@
-import ES6Promise from 'es6-promise';
-import fetch from 'isomorphic-fetch';
-
-ES6Promise.polyfill();
 /**
  * @method spread
  * @description return the promise with the arguments.
