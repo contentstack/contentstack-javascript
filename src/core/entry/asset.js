298d17b85fd99a9d6d914000f48425b857160528
Aamod Pisat <aamod.pisat@raweng.com>
2017-09-29 15:38:59 +0530

    getDownloadUrl)

--- src/core/entry/asset.js
+++ src/core/entry/asset.js
@@ -1,6 +1,36 @@
 import * as Utils from '../lib/utils';
 import Stack from '../stack';
 import Query from './query';
+
+const _extend = function (type) { 
+    return function () {
+        this._query[type] = this._query[type] || {};
+        switch (arguments.length) {
+            case 1:
+            if (Array.isArray(arguments[0]) || typeof arguments[0] === "string") {
+                let query = this._query[type]['BASE'] || [];
+                query = query.concat(arguments[0]);
+                this._query[type]['BASE'] = query;
+                return this;
+            } else {
+                console.error("Kindly provide valid parameters");
+            }
+            break;
+            case 2:
+            if (typeof arguments[0] === "string" && (Array.isArray(arguments[1]) || typeof arguments[1] === "string")) {
+                let query = this._query[type][arguments[0]] || [];
+                query = query.concat(arguments[1]);
+                this._query[type][arguments[0]] = query;
+                return this;
+            } else {
+                console.error("Kindly provide valid parameters");
+            }
+            break;
+            default:
+            console.error("Kindly provide valid parameters");
+        }
+    };
+};
 /**
  * @summary Creates an instance of `Asset`.
  * @description An initializer is responsible for creating Asset object.
@@ -10,32 +40,49 @@ import Query from './query';
  * @returns {Asset}
  * @ignore
  */
-export default class Asset {
-	constructor(){
-		this._query = {};
-		return this;
-	}
-
-	setCacheProvider(provider){
-        if(provider && typeof provider === 'object') {
-            this.provider = provider;
-
-        }
+ export default class Asset {
+   constructor(){
+        this._query = {};
+       /**
+         * @method only
+         * @description This method is use to show the selected fields of the assets in resultset.
+         * @param {String} [key=BASE] - single field in asset
+         * @param {Array} values - array of fields to be show in resultset
+         * @example
+         * <caption> .only with field uid </caption>
+         * Asset().only('title')
+         * @example
+         * <caption> .only with field uid </caption>
+         * Asset().only('BASE','title')
+         * @example
+         * <caption> .only with field uids(array) </caption>
+         * Asset().only(['title','description'])
+         * @returns {Asset}
+         */
+        this.only = _extend('only');
         return this;
-    }
+  }
 
-    setCachePolicy(policy){
-        if(typeof policy === 'number' && policy >= -1 && policy < 4) {
-            if(!this._query) {
-                this.cachePolicy = policy;
-            } else {
-                this.queryCachePolicy = policy;
-            }
+  setCacheProvider(provider){
+    if(provider && typeof provider === 'object') {  
+        this.provider = provider;
+
+    }
+    return this;
+}
+
+setCachePolicy(policy){
+    if(typeof policy === 'number' && policy >= -1 && policy < 4) {
+        if(!this._query) {
+            this.cachePolicy = policy;
         } else {
-            console.error("Kindly provide the valid policy");
+            this.queryCachePolicy = policy;
         }
-        return this;
+    } else {
+        console.error("Kindly provide the valid policy");
     }
+    return this;
+}
 
     /**
      * @method Query
@@ -43,9 +90,27 @@ export default class Asset {
      * @example Assets().Query()
      * @returns {Query}
      */
-    Query(){
+     Query(){
         let query = new Query();
+        console.log("this Query:::::::::::::::", this);
         return Utils.merge(query, this);
+    }
+
+    /**
+     * @method addQuery
+     * @description This method is used to add query to Entry object.
+     * @param {String} key - key of the query
+     * @param {String} value - value of the query
+     * @example Assets().addQuery('include_schema',true)
+     * @returns {Entry}
+     */
+     addQuery(key, value){
+        if (key && value && typeof key === 'string') {
+            this._query[key] = value;
+            return this;
+        } else {
+            console.error("First argument should be a String.");
+        }
     }
 
     /**
@@ -61,10 +126,11 @@ export default class Asset {
      *      })
      * @returns {Object}
      */
-    toJSON(){
+     toJSON(){
         this.tojson = true;
         return this;
     }
+
 
     /**
 	 * @method fetch
@@ -72,20 +138,20 @@ export default class Asset {
 	 * @example
 	 * Stack.Asset('bltsomething123').fetch()
 	 */
-	fetch() {
-		if (this.asset_uid) {
-			this.requestParams = {
-	            method: 'POST',
-	            headers: this.headers,
-	            url: this.config.protocol + "://" + this.config.host + ':' + this.config.port + '/' + this.config.version + this.config.urls.assets + this.asset_uid,
-	            body: {
-	                _method: 'GET',
-	                query: this._query
-	            }
-			}
-			return Utils.sendRequest(this); 
-		} else {
-			console.error("Kindly provide an asset uid. e.g. .Asset('bltsomething123')");
-		}
-	}
+    fetch() {
+      if (this.asset_uid) {
+         this.requestParams = {
+           method: 'POST',
+           headers: this.headers,
+           url: this.config.protocol + "://" + this.config.host + ':' + this.config.port + '/' + this.config.version + this.config.urls.assets + this.asset_uid,
+           body: {
+               _method: 'GET',
+               query: this._query
+           }
+       }
+       return Utils.sendRequest(this); 
+   } else {
+     console.error("Kindly provide an asset uid. e.g. .Asset('bltsomething123')");
+ }
+}
 }
