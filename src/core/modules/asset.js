dae52a788de8ab0b6bf206a5eae8e22712dc465b
Aamod Pisat <aamod.pisat@raweng.com>
2017-10-14 13:23:23 +0530

    added asset support and minor bug fix

--- src/core/modules/asset.js
+++ src/core/modules/asset.js
@@ -1,8 +1,6 @@
 import * as Utils from '../lib/utils';
 import Stack from '../stack';
 import Query from './query';
-<<<<<<< HEAD
-=======
 
 const _extend = function (type) { 
     return function () {
@@ -33,44 +31,11 @@ const _extend = function (type) {
         }
     };
 };
->>>>>>> 5f8e4cdd88f31d7d7d2c98debc826c27c1942e2f
 /**
  * @summary Creates an instance of `Asset`.
  * @description An initializer is responsible for creating Asset object.
  * @param {String} uid - uid of the asset
  * @example
-<<<<<<< HEAD
- * let Entry = Contentstack.Stack().Asset('bltsomething123');
- * @returns {Asset}
- * @ignore
- */
-export default class Asset {
-	constructor(){
-		this._query = {};
-		return this;
-	}
-
-	setCacheProvider(provider){
-        if(provider && typeof provider === 'object') {
-            this.provider = provider;
-
-        }
-        return this;
-    }
-
-    setCachePolicy(policy){
-        if(typeof policy === 'number' && policy >= -1 && policy < 4) {
-            if(!this._query) {
-                this.cachePolicy = policy;
-            } else {
-                this.queryCachePolicy = policy;
-            }
-        } else {
-            console.error("Kindly provide the valid policy");
-        }
-        return this;
-    }
-=======
  * let Entry = Contentstack.Stack().Assets('bltsomething123');
  * @returns {Asset}
  * @ignore
@@ -96,7 +61,6 @@ export default class Asset {
          this.only = _extend('only');
          return this;
      }
->>>>>>> 5f8e4cdd88f31d7d7d2c98debc826c27c1942e2f
 
     /**
      * @method Query
@@ -104,26 +68,12 @@ export default class Asset {
      * @example Assets().Query()
      * @returns {Query}
      */
-<<<<<<< HEAD
-    Query(){
-=======
      Query(){
->>>>>>> 5f8e4cdd88f31d7d7d2c98debc826c27c1942e2f
         let query = new Query();
         return Utils.merge(query, this);
     }
 
     /**
-<<<<<<< HEAD
-     * @method toJSON
-     * @description This method is used to convert the result in to plain javascript object.
-     * @example
-     * blogEntry
-     *      .toJSON()
-     *      .then(function (result) {
-     *          let value = result.get(field_uid)
-    *       },function (error) {
-=======
      * @method addQuery
      * @description This method is used to add query to Entry object.
      * @param {String} key - key of the query
@@ -149,50 +99,22 @@ export default class Asset {
      *      .then(function (result) {
      *          let value = result.get(field_uid)
      *       },function (error) {
->>>>>>> 5f8e4cdd88f31d7d7d2c98debc826c27c1942e2f
      *          // error function
      *      })
      * @returns {Object}
      */
-<<<<<<< HEAD
-    toJSON(){
-=======
      toJSON(){
->>>>>>> 5f8e4cdd88f31d7d7d2c98debc826c27c1942e2f
         this.tojson = true;
         return this;
     }
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 5f8e4cdd88f31d7d7d2c98debc826c27c1942e2f
     /**
-	 * @method fetch
-	 * @description fetch asset obhect of requested Asset uid of defined query if present.
-	 * @example
-<<<<<<< HEAD
-	 * Stack.Asset('bltsomething123').fetch()
-	 */
-	fetch() {
-		if (this.asset_uid) {
-			this.requestParams = {
-	            method: 'POST',
-	            headers: this.headers,
-	            url: this.config.protocol + "://" + this.config.host + ':' + this.config.port + '/' + this.config.version + this.config.urls.assets + this.asset_uid,
-	            body: {
-	                _method: 'GET',
-	                query: this._query
-	            }
-			}
-			return Utils.sendRequest(this); 
-		} else {
-			console.error("Kindly provide an asset uid. e.g. .Asset('bltsomething123')");
-		}
-	}
-=======
-	 * Stack.Assets('bltsomething123').fetch()
-	 */
+     * @method fetch
+     * @description fetch asset obhect of requested Asset uid of defined query if present.
+     * @example
+     * Stack.Assets('bltsomething123').fetch()
+     */
      fetch() {
           if (this.asset_uid) {
                this.requestParams = {
@@ -209,5 +131,4 @@ export default class Asset {
             console.error("Kindly provide an asset uid. e.g. .Assets('bltsomething123')");
         }
     }
->>>>>>> 5f8e4cdd88f31d7d7d2c98debc826c27c1942e2f
 }
