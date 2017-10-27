3c9fdd8e122415588f112868979c397200b62e3b
Hitesh Baldaniya <hiteshbal91@gmail.com>
2017-10-27 17:27:05 +0530

    Features: image transformation
    Changes:
    deprecated .findOne() from Query
    deprecated .includeSchema() from Query

--- webpack/webpack.web.js
+++ webpack/webpack.web.js
@@ -1,29 +1,27 @@
 'use strict';
 
 const path = require('path');
-const webpackMerge = require('webpack-merge'); 
+const webpackMerge = require('webpack-merge');
 
 const commonConfig = require('./webpack.common.js');
 
-module.exports = function (options) {
-	return webpackMerge(commonConfig(),{
-		output: {
-		    library: "Contentstack",
-		    libraryTarget: "umd",
-		    path: path.join(__dirname, "../dist/web"),
-		    filename: "contentstack.js"
-		},
-		resolve: {
-			alias:{
-		      runtime: path.resolve( __dirname,  '../src/runtime/web')
-		    },
-			modules: [
-				'../src', 
-				'../src/runtimes/web',
-				'node_modules'
-			]
-		}
-	});
+module.exports = function(options) {
+    return webpackMerge(commonConfig(), {
+        output: {
+            library: "Contentstack",
+            libraryTarget: "umd",
+            path: path.join(__dirname, "../dist/web"),
+            filename: "contentstack.js"
+        },
+        resolve: {
+            alias: {
+                runtime: path.resolve(__dirname, '../src/runtime/web')
+            },
+            modules: [
+                '../src',
+                '../src/runtimes/web',
+                'node_modules'
+            ]
+        }
+    });
 }
-
-
