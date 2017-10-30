4767d16331a341c479e131b6dd6d5a39283b6e49
Hitesh Baldaniya <hiteshbal91@gmail.com>
2017-10-30 18:42:56 +0530

    ImageTransform added in readme.md web testing done

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -4,48 +4,37 @@ const webpack = require('webpack');
 const path = require('path');
 
 const Package = require('./../package.json');
-  
-module.exports = function (options) {
-  return {
 
-    entry: {
-      contentstack: "./src/core/contentstack",
-    },
-
-    resolve: {
-      extensions: ['.js']
-    },
-
-    module: {
-      rules: [
-       {
-          test: /\.js?$/,
-          exclude: ['../node_modules'],
-          use: [
-            {
-              loader:'babel-loader',
-              options: {
-                presets: ['es2015'],
-              }
-            },
-            {
-              loader:'string-replace-loader',
-              query: {
-                search: '{{VERSION}}',
-                replace: Package.version
-              }
-            }
-          ],
-        }
-      ]
-
-    },
-
-    plugins: [
-       new webpack.IgnorePlugin(/vertx/),
-     ]
-
-  };
+module.exports = function(options) {
+    return {
+        entry: {
+            contentstack: "./src/core/contentstack",
+        },
+        resolve: {
+            extensions: ['.js']
+        },
+        module: {
+            rules: [{
+                test: /\.js?$/,
+                exclude: ['../node_modules'],
+                use: [{
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015'],
+                        }
+                    },
+                    {
+                        loader: 'string-replace-loader',
+                        query: {
+                            search: '{{VERSION}}',
+                            replace: Package.version
+                        }
+                    }
+                ],
+            }]
+        },
+        plugins: [
+            new webpack.IgnorePlugin(/vertx/),
+        ]
+    };
 }
-
-
