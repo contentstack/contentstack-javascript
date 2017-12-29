01087146d250743ea8d12ee61003f2d4e4eb295c
Rohit Mishra <Rohit>
2017-12-29 15:55:59 +0530

    Updated the sdk with new method

--- test/config.js
+++ test/config.js
@@ -2,16 +2,16 @@
 
 module.exports = {
     stack: {
-        "api_key": "bltc0a6a8609e24c651",
-        "access_token": "blt882b9ae3ee9af2e1",
-        "environment": "development"
+        "api_key": "<API-TOKEN>",
+        "access_token": "<ACCESS-TOKEN>",
+        "environment": "<ENVIRONMENT>"
     },
     host: "cdn.contentstack.io",
     url: "https://api.contentstack.io/v3",
     runscope: {
         url: "https://api.runscope.com/radar/76730163-fab9-4554-95c1-7ba75aaa2628/trigger?runscope_environment=1f5114ea-7a2d-4adf-9fd3-abc885c47783",
-        username: "jayesh.sawant@raweng.com",
-        password: "suraj@123"
+        username: "<USERNAME>",
+        password: "<PASSWORD>"
     },
     contentTypes: {
         source: "source",
@@ -22,8 +22,8 @@ module.exports = {
         port: 465,
         secure: true,
         auth: {
-            user: 'rohit.mishra@raweng.com',
-            pass: 'rohitmishra209'
+            user: '<USERNAME>',
+            pass: '<PASSWORD>'
         }
     }
 }
