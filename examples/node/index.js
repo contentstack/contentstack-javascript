cccaabb9a4af82bb0098eb4e11735ffbcbc21001
Rohit Mishra <Rohit>
2018-01-16 15:04:40 +0530

    Updated the modules

--- examples/node/index.js
+++ examples/node/index.js
@@ -2,11 +2,25 @@
 
 const ContentstackDemo = require('./contentstack-demo.js')
 
-const Demo = new ContentstackDemo({ 'api_key': 'bltsomething123', 'access_token': 'bltsomething123', 'environment': 'development' })
+const Demo = new ContentstackDemo({ 'api_key': 'blt7226fe8124ef0a4a', 'access_token': 'blt6e288386d4a8e18a', 'environment': 'development' })
 
-//get all the entries
+
+    // Demo
+    // .getEntries()
+    // .then(function(result) {
+    //     // result object with entry
+    //     console.info("Result2 : ", JSON.stringify(result))
+    // })
+    // .catch(function(err) {
+    //     // error of get entry
+    //     console.error("Fetch Error :", err)
+    // })
+
+
+
+    //get all the entries
 Demo
-    .getEntries('source')
+    .getEntries('homepage')
     .spread(function(result) {
         // result object with entries
         console.info("Result: ", result)
@@ -17,46 +31,4 @@ Demo
         console.error("Find Error :", err)
     })
 
-// get single entry
-Demo
-    .getEntry('source', 'bltsomething123')
-    .then(function(result) {
-        // result object with entry
-        console.info("Result2 : ", JSON.stringify(result))
-    })
-    .catch(function(err) {
-        // error of get entry
-        console.error("Fetch Error :", err)
-    })
 
-// get single asset
-Demo
-    .getAsset('bltsomething123')
-    .then(function(result) {
-        // result object with entry
-        console.info("Result2 : ", result)
-    })
-    .catch(function(err) {
-        // error of get entry
-        console.error("Fetch Error :", err)
-    })
-
-//  get all assets
-Demo
-    .getAssets()
-    .spread(function(result) {
-        // result object with entry
-        console.info("Result2 : ", result)
-        for (let i = 0, _i = result.length; i < _i; i++) {
-            // Image optimization
-            const imgUrl = Demo.Stack.imageTransform(result[i]['url'], {
-                quality: 50,
-                format: 'jpg'
-            })
-            console.log("Image URL : ", imgUrl)
-        }
-    })
-    .catch(function(err) {
-        // error of get entry
-        console.error("getAssets Fetch Error :", err)
-    })
