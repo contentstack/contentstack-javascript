e58dd1d2a5b698460708c8c9ae438b11ae96cca7
Rohit Mishra <Rohit>
2018-01-25 15:24:50 +0530

    Added the missing file of Nativescript module

---
+++ webpack/webpack.nativescript.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const path = require('path');
+const webpackMerge = require('webpack-merge');
+
+const commonConfig = require('./webpack.common.js');
+
+module.exports = function(options) {
+    return webpackMerge(commonConfig(), {
+        output: {
+            library: "Contentstack",
+            libraryTarget: "commonjs2",
+            path: path.join(__dirname, "../dist/nativescript"),
+            filename: "contentstack.js"
+        },
+        resolve: {
+            alias: {
+                runtime: path.resolve(__dirname, '../src/runtime/nativescript')
+            },
+            modules: [
+                '../src',
+                '../src/runtimes/native-script',
+                'node_modules',
+            ]
+        },
+        module: {
+            rules: [{
+                test: /\.js?$/,
+                exclude: ['../node_modules'],
+                use: [{
+                    loader: 'string-replace-loader',
+                    query: {
+                        search: '{{PLATFORM}}',
+                        replace: 'react-native'
+                    }
+                }],
+            }]
+        }
+    });
+}
