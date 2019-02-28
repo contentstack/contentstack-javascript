7a3beae0030dafd59f746240d65d29e10e722d48
Uttam K Ukkoji <uttamukkoji@gmail.com>
2019-02-28 18:07:21 +0530

    Cache issue implementation

--- src/runtime/web/localstorage.js
+++ src/runtime/web/localstorage.js
@@ -1,1 +1,16 @@
-export default window.localStorage;
+// export default window.localStorage;
+
+let webLocalStoreage = function (){
+    try {
+        var storage = window.localStorage,
+            x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return window[type];
+    } catch(e) {
+        return null
+    }
+};
+
+export default webLocalStoreage
+    
