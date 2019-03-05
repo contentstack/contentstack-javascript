9a23829115a7acc722d65496e5bad6fbcebbf563
Rohit Mishra <rohit.mishra@raweng.com>
2019-03-05 18:09:54 +0530

    Added two new methods and solved the cachepolicy

--- src/runtime/web/localstorage.js
+++ src/runtime/web/localstorage.js
@@ -2,15 +2,15 @@
 
 let webLocalStoreage = function (){
     try {
-        var storage = window.localStorage,
-            x = '__storage_test__';
-        storage.setItem(x, x);
-        storage.removeItem(x);
-        return window[type];
+        var storage = window.localStorage
+        // x = '__storage_test__';
+        // storage.setItem(x, x);
+        // storage.removeItem(x);
+        return storage;
     } catch(e) {
         return null
     }
 };
 
-export default webLocalStoreage
+export default webLocalStoreage();
     
