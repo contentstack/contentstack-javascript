0c4dc034d31eb0c9227f157f021222619d1d98ce
Rohit Mishra <rohit.mishra@raweng.com>
2019-04-12 16:18:22 +0530

    supporting two new methods

--- test/sync_config.js
+++ test/sync_config.js
@@ -2,9 +2,9 @@
 
 module.exports = {
     stack: {
-        "api_key": "blt477ba55f9a67bcdf",
-        "access_token": "cs7731f03a2feef7713546fde5",
-        "environment": "web"
+        "api_key": "",
+        "access_token": "",
+        "environment": ""
     },
     host: "cdn.contentstack.io",
     url: "https://api.contentstack.io/v3",
