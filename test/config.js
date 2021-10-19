4b6cb98340594bf710e1a1cb7eb28291765e3a20
Uttam Krishna Ukkoji <uttamukkoji@gmail.com>
2021-10-19 23:12:37 +0530

    Removed issues

--- test/config.js
+++ test/config.js
@@ -6,12 +6,12 @@ module.exports = {
         "delivery_token": "",
         "environment": ""
     },
-    host: "cdn.contentstack.io",
-    url: "https://api.contentstack.io/v3",
+    host: "",
+    url: "",
     runscope: {
-        url: "https://api.runscope.com/radar/76730163-fab9-4554-95c1-7ba75aaa2628/trigger?runscope_environment=1f5114ea-7a2d-4adf-9fd3-abc885c47783",
-        username: "aamod.pisat@raweng.com",
-        password: "cs@aamod$123",
+        url: "",
+        username: "",
+        password: "",
     },
     contentTypes: {
         source: "source",
--- test/sync_config.js
+++ test/sync_config.js
@@ -6,13 +6,8 @@ module.exports = {
         "delivery_token": "",
         "environment": ""
     },
-    host: "cdn.contentstack.io",
-    url: "https://api.contentstack.io/v3",
-     // runscope: {
-     //     url: "https://api.runscope.com/radar/76730163-fab9-4554-95c1-7ba75aaa2628/trigger?runscope_environment=1f5114ea-7a2d-4adf-9fd3-abc885c47783",
-     //     username: "<USERNAME>",
-     //     password: "<PASSWORD>"
-     // },
+    host: "",
+    url: "",
     contentTypes: {
         source: "source",
         numbers_content_type: "numbers_content_type"
