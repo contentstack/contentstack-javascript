8e7053191e51b0dc091545e3ecd3382718e6414a
Uttam Krishna Ukkoji <uttamukkoji@gmail.com>
2021-10-21 17:15:11 +0530

    test: :white_check_mark: Ran test for api and ts

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   forceExit:true,
+  transform: {
+    "^.+\\.(t|j)sx?$": "ts-jest",
+    "^.+\\.(ts|tsx|js|jsx)$": "babel-jest"
+  },
   reporters: [
     "default",
     ["jest-html-reporters", 
