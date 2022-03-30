5b4ba0a82c09efed550f167b41b9f1a30ace4487
Uttam Krishna Ukkoji <uttamukkoji@gmail.com>
2022-03-30 13:51:01 +0530

    test case update

---
+++ test/sync/sync-testcases.js
@@ -0,0 +1,108 @@
+'use strict';
+/*
+ * Module Dependencies.
+ */
+const sync_testcase = require('tape');
+const Contentstack = require('../../dist/node/contentstack.js');
+const init = require('../sync_config.js');
+
+let Stack;
+let sync_token = "";
+var total_count = 123;
+var pagination_token = "";
+
+
+/*
+ * Initalise the Contentstack Instance
+ * */
+sync_testcase('Initalise the Contentstack Stack Instance', function(TC) {
+    setTimeout(function() {
+        Stack = Contentstack.Stack(init.stack);
+        Stack.setHost(init.host);
+        TC.end();
+    }, 1000);
+});
+
+sync_testcase('default .Init()', function(assert) {
+    Stack
+         .sync({"init" : true})
+         .then(function success(data) {
+          assert.equal(data.total_count, total_count, "Present Data and Totalcount is equal");     	
+          assert.end();
+         });
+});
+
+sync_testcase('default .startdate()', function(assert) {
+   var date_entry_count = 7
+    Stack
+         .sync({"init": true, "start_from": "2018-10-22"})
+         .then(function success(data) {
+             assert.equal(data.total_count, date_entry_count, "Present data and filtered data count on date bases is equal");       
+             assert.end();
+         });
+});
+
+
+sync_testcase('default .locale()', function(assert) {
+    var locale_entry_count = 123;
+    Stack
+         .sync({"init": "true", "locale": "en-us"})
+         .then(function success(data) {     
+            assert.equal(data.total_count, locale_entry_count, "Present data and filtered data count on locale bases is equal");       
+            assert.end();
+         });
+});
+
+sync_testcase('default .localeDate()', function(assert) {
+    var locale_date_entry_count = 7;
+    Stack
+         .sync({"init": true, "locale": "en-us", "start_from": "2018-10-22"})
+         .then(function success(data) {
+            assert.equal(data.total_count, locale_date_entry_count, "Present data and filtered data count on date and locale bases is equal");       
+             assert.end();
+         });
+});
+
+
+sync_testcase('default .pagination_token()', function(assert) {
+
+    Stack
+           .sync({"pagination_token" : pagination_token})
+           .then(function success(result) {
+            let pagination_count = result.items.length
+                assert.equal(pagination_count, 23, "pagination_token testcase executed successfully");  
+                assert.end();
+           });  
+});
+
+
+sync_testcase('default .contentTypeUid()', function(assert) {
+    Stack
+         .sync({"init": true, "content_type_uid": "session"})
+         .then(function success(data) {
+            assert.equal(data.total_count, 31, "Present data and filtered data total count on contentType bases is equal");       
+            assert.end();
+         });
+});
+
+sync_testcase('default .type()', function(assert) {
+    var items_count = 8;
+    Stack
+         .sync({"init": true, "type": "asset_published"})
+         .then(function success(data) {
+            assert.equal(data.total_count, items_count, "Present data and filtered data total count on type bases is equal");       
+            assert.end();
+         });
+});
+
+sync_testcase('default .sync_token()', function(assert) {
+   var sync_expected_count = 7 
+
+   Stack
+         .sync({"sync_token" :  sync_token})
+         .then(function success(result) {
+          assert.equal(result.total_count, sync_expected_count, "Synced Data and Sync_total_count is equal");      
+          assert.end();
+         });
+});
+
---
+++ test/typescript/entry.test.ts
@@ -0,0 +1,132 @@
+import * as Contentstack from '../..';
+
+const stack = Contentstack.Stack({ api_key: 'api_key', delivery_token: 'delivery_token', environment: 'environment'});
+
+describe('Entry Test', () => {
+    test('Entry with UID', done => {
+        const entry = makeEntry();        
+        expect(entry.entry_uid).toEqual('entryUID');
+        expect(entry.content_type_uid).toEqual('uid');
+        expect(entry.except).not.toEqual(undefined);
+        expect(entry.only).not.toEqual(undefined);
+        expect(entry.setCacheProvider).not.toEqual(undefined);
+        expect(entry.setCachePolicy).not.toEqual(undefined);
+        expect(entry.includeReference).not.toEqual(undefined);
+        expect(entry.language).not.toEqual(undefined);
+        expect(entry.addQuery).not.toEqual(undefined);
+        expect(entry.includeSchema).not.toEqual(undefined);
+        expect(entry.includeReference).not.toEqual(undefined);
+        expect(entry.includeReferenceContentTypeUID).not.toEqual(undefined);
+        expect(entry.includeContentType).not.toEqual(undefined);
+        expect(entry.includeOwner).not.toEqual(undefined);
+        expect(entry.toJSON).not.toEqual(undefined);
+        expect(entry.addParam).not.toEqual(undefined);
+        done();
+    });
+    
+    test('Entry fetch test', done => {
+        makeEntry().fetch().then((response) => done()).catch((error) => done());
+    });
+
+    test('Entry only test', done => {
+        const entry = makeEntry().only('fieldUID');
+        expect(entry._query).toEqual({ "only": { "BASE": ["fieldUID"]}});
+        const entry1 = makeEntry().only('BASE','fieldUID');
+        expect(entry1._query).toEqual({ "only": { "BASE": ["fieldUID"]}});
+        const entry2 = makeEntry().only(['fieldUID', 'fieldUID1']);
+        expect(entry2._query).toEqual({ "only": { "BASE": ["fieldUID", "fieldUID1"]}});
+        const entry3 = makeEntry().only('referenceUID', 'fieldUID');
+        expect(entry3._query).toEqual({ "only": { "referenceUID": ["fieldUID"]}});
+        const entry4 = makeEntry().only('referenceUID', ['fieldUID', 'fieldUID1']);
+        expect(entry4._query).toEqual({ "only": { "referenceUID": ["fieldUID", "fieldUID1"]}});
+        done();
+    });
+
+    test('Entry except test', done => {
+        const entry = makeEntry().except('fieldUID');
+        expect(entry._query).toEqual({ "except": { "BASE": ["fieldUID"]}});
+        const entry1 = makeEntry().except('BASE','fieldUID');
+        expect(entry1._query).toEqual({ "except": { "BASE": ["fieldUID"]}});
+        const entry2 = makeEntry().except(['fieldUID', 'fieldUID1']);
+        expect(entry2._query).toEqual({ "except": { "BASE": ["fieldUID", "fieldUID1"]}});
+        const entry3 = makeEntry().except('referenceUID', 'fieldUID');
+        expect(entry3._query).toEqual({ "except": { "referenceUID": ["fieldUID"]}});
+        const entry4 = makeEntry().except('referenceUID', ['fieldUID', 'fieldUID1']);
+        expect(entry4._query).toEqual({ "except": { "referenceUID": ["fieldUID", "fieldUID1"]}});
+        done();
+    });
+
+    test('Entry Cache Provider test', done => {
+        const entry = makeEntry().setCachePolicy(Contentstack.CachePolicy.CACHE_ELSE_NETWORK);
+        expect(entry.queryCachePolicy).toEqual(Contentstack.CachePolicy.CACHE_ELSE_NETWORK);
+        done()
+    });
+
+    test('Entry include reference test', done => {
+        const entry = makeEntry().includeReference('referenceUID');
+        expect(entry._query).toEqual({"include": ["referenceUID"]});
+
+        const entry1 = makeEntry().includeReference(['referenceUID', 'referenceUID1']);
+        expect(entry1._query).toEqual({"include": ["referenceUID", 'referenceUID1']});
+        done()
+    });
+
+    test('Entry locale test', done => {
+        const entry = makeEntry().language('en-us');
+        expect(entry._query).toEqual({"locale": 'en-us'});
+        done()
+    });
+
+    test('Entry add param test', done => {
+        const entry = makeEntry().addQuery('key', 'value')
+        expect(entry._query).toEqual({"key": 'value'});
+        done()
+    });
+
+    test('Entry add param test', done => {
+        const entry = makeEntry().addParam('key', 'value')
+        expect(entry._query).toEqual({"key": 'value'});
+        done()
+    });
+
+
+    test('Entry include schema test', done => {
+        const entry = makeEntry().includeEmbeddedItems()
+        expect(entry._query).toEqual({"include_embedded_items": ['BASE']});
+        done()
+    });
+
+    test('Entry include schema test', done => {
+        const entry = makeEntry().includeSchema()
+        expect(entry._query).toEqual({"include_schema": true});
+        done()
+    });
+
+    test('Entry include content type test', done => {
+        const entry = makeEntry().includeContentType()
+        expect(entry._query).toEqual({"include_content_type": true});
+        done()
+    });
+
+    test('Entry include reference content type test', done => {
+        const entry = makeEntry().includeReferenceContentTypeUID()
+        expect(entry._query).toEqual({"include_reference_content_type_uid": true});
+        done()
+    });
+
+    test('Entry include fallback test', done => {
+        const entry = makeEntry().includeFallback()
+        expect(entry._query).toEqual({"include_fallback": true});
+        done()
+    });
+
+    test('Entry include owner test', done => {
+        const entry = makeEntry().includeOwner()
+        expect(entry._query).toEqual({"include_owner": true});
+        done()
+    });
+});
+
+function makeEntry() {
+    return stack.ContentType('uid').Entry('entryUID')
+}
