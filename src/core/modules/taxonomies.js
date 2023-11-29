d26ace238ddef23c97f5734da6fe7e2ad918544e
Abhinav Gupta <abhinav.gupta@contentstack.com>
2023-11-29 10:58:20 +0530

    WIP: taxonomy tests

--- src/core/modules/taxonomies.js
+++ src/core/modules/taxonomies.js
@@ -1,1 +1,14 @@
-import config from '../../../'
+import * as Utils from '../lib/utils';
+
+
+export default class Taxonomies {
+  constructor() {
+    this._query = {};
+    return this;
+  }
+
+  toJSON() {
+    this.toJSON = true;
+    return this;
+  }
+}
