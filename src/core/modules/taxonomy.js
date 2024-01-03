9598402570dd310f40113dcf611a5de3e8049e4a
Abhinav Gupta <abhinav.gupta@contentstack.com>
2024-01-03 16:10:24 +0530

    fix: fixed taxonomies url, levels param and added doc

--- src/core/modules/taxonomy.js
+++ src/core/modules/taxonomy.js
@@ -3,12 +3,12 @@ import Query from "./query";
 // Overrideing compare function to include level
 const _extend = {
   compare: function(type) {
-    return function(key, value, level=0) {
+    return function(key, value, levels) {
         if (key && value && typeof key === 'string' && typeof value !== 'undefined') {
             this._query['query'][key] = this._query['query']['file_size'] || {};
             this._query['query'][key][type] = value;
-            if (level > 0 && level <= 10) {
-              this._query['query'][key]['level'] = level
+            if (levels && typeof levels === "number") {
+              this._query['query'][key]['levels'] = levels
             }
             return this;
         } else {
@@ -21,9 +21,84 @@ const _extend = {
 export default class TaxonomyQuery extends Query {
   constructor() {
     super();
+    /**
+     * @method above
+     * @memberOf Query
+     * @description Get all entries for a specific taxonomy that match only the parent term(s) of a specified target term, excluding the target term itself. You can also specify a specific level.
+     * @param {String} key - uid of the taxonomy, specified as `taxonomies.<taxonomy_uid>`
+     * @param {*} value - uid of the term to be matched
+     * @example For taxonomy_uid = taxonomy1, and term_uid = term1
+     * let blogQuery = Stack().ContentType('example').Query();
+     *          let data = blogQuery.above("taxonomies.taxonomy1", "term1").toJSON().find() // without levels
+     *          let data = blogQuery.above("taxonomies.taxonomy1", "term1", 4).toJSON().find() // with levels
+     *          data.then(function (result) {
+     *          // result = the data which matches only the parent term(s) of the specified term, excluding the term itself
+     *       },function (error) {
+     *          // error function
+     *      })
+     * @returns {Query}
+     * @instance
+     */
     this.above = _extend.compare('$above')
+
+    /**
+     * @method equalAndAbove
+     * @memberOf Query
+     * @description Get all entries for a specific taxonomy that match a specific term and all its ancestor terms, requiring only the target term and a specified level.
+     * @param {String} key - uid of the taxonomy, specified as `taxonomies.<taxonomy_uid>`
+     * @param {*} value - uid of the term to be matched
+     * @example For taxonomy_uid = taxonomy1, and term_uid = term1
+     * let blogQuery = Stack().ContentType('example').Query();
+     *          let data = blogQuery.equalAndAbove("taxonomies.taxonomy1", "term1").toJSON().find() // without levels
+     *          let data = blogQuery.equalAndAbove("taxonomies.taxonomy1", "term1", 4).toJSON().find() // with levels
+     *          data.then(function (result) {
+     *          // result = the data which matches a specific term and all its ancestor terms
+     *       },function (error) {
+     *          // error function
+     *      })
+     * @returns {Query}
+     * @instance
+     */
     this.equalAndAbove = _extend.compare('$eq_above')
+    
+    /**
+     * @method below
+     * @memberOf Query
+     * @description Get all entries for a specific taxonomy that match all of their descendant terms by specifying only the target term and a specific level.
+     * @param {String} key - uid of the taxonomy, specified as `taxonomies.<taxonomy_uid>`
+     * @param {*} value - uid of the term to be matched
+     * @example For taxonomy_uid = taxonomy1, and term_uid = term1
+     * let blogQuery = Stack().ContentType('example').Query();
+     *          let data = blogQuery.below("taxonomies.taxonomy1", "term1").toJSON().find() // without levels
+     *          let data = blogQuery.below("taxonomies.taxonomy1", "term1", 4).toJSON().find() // with levels
+     *          data.then(function (result) {
+     *          // result = the data which matches all of the descendant terms.
+     *       },function (error) {
+     *          // error function
+     *      })
+     * @returns {Query}
+     * @instance
+     */
     this.below = _extend.compare('$below')
+    
+    /**
+     * @method equalAndBelow
+     * @memberOf Query
+     * @description Get all entries for a specific taxonomy that match a specific term and all its descendant terms, requiring only the target term and a specified level.
+     * @param {String} key - uid of the taxonomy, specified as `taxonomies.<taxonomy_uid>`
+     * @param {*} value - uid of the term to be matched
+     * @example For taxonomy_uid = taxonomy1, and term_uid = term1
+     * let blogQuery = Stack().ContentType('example').Query();
+     *          let data = blogQuery.equalAndBelow("taxonomies.taxonomy1", "term1").toJSON().find() // without levels
+     *          let data = blogQuery.equalAndBelow("taxonomies.taxonomy1", "term1", 4).toJSON().find() // with levels
+     *          data.then(function (result) {
+     *          // result = the data which matches a specific term and all its descendant terms.
+     *       },function (error) {
+     *          // error function
+     *      })
+     * @returns {Query}
+     * @instance
+     */
     this.equalAndBelow = _extend.compare('$eq_below')
   }
 }
