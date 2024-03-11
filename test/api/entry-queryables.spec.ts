f3a0065222408c882b23e0500210132dc556426c
harshithad0703 <harshitha.d@contentstack.com>
2024-03-11 11:25:36 +0530

    test: :white_check_mark: added unit and api tests for or operator

--- test/api/entry-queryables.spec.ts
+++ test/api/entry-queryables.spec.ts
@@ -1,6 +1,8 @@
 import { stackInstance } from '../utils/stack-instance';
 import { Entries } from '../../src/lib/entries';
 import { TEntry } from './types';
+import { QueryOperation } from 'src/lib/types';
+import { Query } from 'src/lib/query';
 
 const stack = stackInstance();
 
@@ -35,10 +37,26 @@ describe('Query Operators API test cases', () => {
         expect((query.entries[0] as any).multi_line).not.toBeDefined()
       }
     });
-  });
+    it('should return entries matching any of the conditions', async () => {
+        const query1: Query = await makeEntries('contenttype_uid').query().containedIn('title', ['value']);
+        const query2: Query = await makeEntries('contenttype_uid').query().where('title', QueryOperation.EQUALS, 'value2');
+        const query = await makeEntries('contenttype_uid').query().or(query1, query2).find<TEntry>();
+      
+        if (query.entries) {
+          expect(query.entries).toHaveLength(2);
+          expect(query.entries[0]._version).toBeDefined();
+          expect(query.entries[0].locale).toBeDefined();
+          expect(query.entries[0].uid).toBeDefined();
+          expect(query.entries[0].title).toBe('value2');
+          expect(query.entries[1]._version).toBeDefined();
+          expect(query.entries[1].locale).toBeDefined();
+          expect(query.entries[1].uid).toBeDefined();
+          expect(query.entries[1].title).toBe('value');
+        }
+    });
+});
   
-  function makeEntries(contentTypeUid = ''): Entries {
+function makeEntries(contentTypeUid = ''): Entries {
     const entries = stack.ContentType(contentTypeUid).Entry();
-  
     return entries;
-  }
+}
