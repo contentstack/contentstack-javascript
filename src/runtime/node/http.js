82afc27a58299c4048f6bfada4ebe56eea7fef28
harshitha.d <harshitha.d@contentstack.com>
2024-12-16 16:51:54 +0530

    dropping isomorphic-fetch and node-fetch

--- src/runtime/node/http.js
+++ src/runtime/node/http.js
@@ -1,6 +1,6 @@
 import ES6Promise from 'es6-promise';
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch'; 
 
 ES6Promise.polyfill();
 
-export default fetch;
+export default fetch; // fetch API available in Node.js 18 and later
