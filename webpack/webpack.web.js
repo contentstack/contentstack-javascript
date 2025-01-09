a0495bf1dbe63fc70a24921d87a28a4c44206ea6
Vikram Kalta <vikramkalta@Vikrams-MacBook-Pro.local>
2025-01-09 22:47:33 +0000

    fix: setting minimize to true for browser

--- webpack/webpack.web.js
+++ webpack/webpack.web.js
@@ -54,6 +54,9 @@ module.exports = function(options) {
             new webpack.ProvidePlugin({
                 global: require.resolve('./../global.js')
               })
-        ]
+        ],
+        optimization: {
+            minimize: true,
+        },
     });
 }
